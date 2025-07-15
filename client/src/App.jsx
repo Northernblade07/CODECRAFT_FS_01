652abd7c49a7026d4c5917de5ba2e67d56b8ef7d
Prahant Bhandari <prashantbhandaricr07@gmail.com>
2025-07-15 16:55:55 +0530

    setup app file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
+import { Home } from './pages/Home';
+import { ToastContainer } from 'react-toastify';
+import { useContext } from 'react';
+import { AppContext } from './context/AppContext';
+import AdminRoute from './components/AdminRoutes';
 
-const App = () => {
+
+export const App = () => {
+ const {userData ,isLoggedin,} = useContext(AppContext);
+ console.log(userData?.role)
+ console.log(isLoggedin)
+//  const isAdmin = userData.role==='admin';
   return (
-    <div>App</div>
+    <div>
+      <ToastContainer />
+      <Routes>
+        <Route path='/' element={isLoggedin?<Home/>:<Navigate to={'/auth'}/>} />
+        <Route path='/auth' element={!isLoggedin?<Login />:<Navigate to={'/'}/>} />
+        <Route path='/admin' element={
+          <AdminRoute>
+            <Home/>
+          </AdminRoute>
+          } />
+
+      </Routes>
+    </div>
   )
 }
 
-export default App
+export default App;
