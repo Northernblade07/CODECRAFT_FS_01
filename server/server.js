f5aaeea9995e20f9c420d3cc66aab00b44ea66ba
Prahant Bhandari <prashantbhandaricr07@gmail.com>
2025-07-15 17:01:58 +0530

    initialize backend

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+import authRouter from './routes/authRoutes.js'
+
+const app = express();
+const port = process.env.PORT || 4000
+
+const allowedOrigins = ['http://localhost:5173']
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
+
+// API Endpoints
+app.get("/", (req, res) => res.send("API Working "));
+app.use('/api/auth', authRouter)
+
+app.listen(port, () => console.log(`Server started on PORT:${port}`));
